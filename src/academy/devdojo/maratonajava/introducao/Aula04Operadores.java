20ab6535abe268060cc02fe32a7885da67ab6087
Janilto Oliveira <janiltojack@yahoo.com.br>
2022-08-17 19:11:17 -0300

    Ajustando o operadores Lógicos

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -7,5 +7,24 @@ public class Aula04Operadores {
         double numero2 = 20;
         double resultado = numero2 / numero1;
         System.out.println(resultado);
+
+        //Resto  %
+        int resto = 21 % 7;
+        System.out.println(resto);
+
+        /*operadores lógicos
+        < menor     >  maior   <= menor igual
+        >= maior igual    == giaul   != diferente
+         */
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezDiferenteDez = 10 != 10;
+        System.out.println("isDezMaiorQueVinte "+isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte "+isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte "+isDezIgualVinte);
+        System.out.println("isDezIgualDez "+isDezIgualDez);
+        System.out.println("isDezDiferenteDez "+isDezDiferenteDez);
     }
 }
