20a240beca3a786efbfc48da01eb78131e6b1aab
Janilto Oliveira <janiltojack@yahoo.com.br>
2022-08-17 12:21:51 -0300

    Ajustando variáveis

--- src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -8,10 +8,11 @@ confirmo que recebi o salário de <salario> na data <data>
 */
 public class Aula03TiposPrimitivosExercicio {
     public static void main(String[] args) {
-        String nome = "Janilto Oliveira";
+        String nome = "Janilto Oliveira da Silva";
         String endereco = "Rua Sucena Chaia Sardela, 156 - Cehab - Itaperuna-RJ";
         double salarioDouble = 5000.25;
         String dateRecebeu = "16/08/2020";
+
         String relatorio = "Eu "+nome+" morando no endereço: "+endereco+" confirmo que recebi o salário de R$ "+salarioDouble+" na data: "+dateRecebeu+".";
 
         System.out.println(relatorio);
