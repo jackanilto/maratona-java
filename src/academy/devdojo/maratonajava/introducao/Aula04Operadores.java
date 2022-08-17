e23de6f3ccc3ef3c45d4aad693be33f84b5b956b
Janilto Oliveira <janiltojack@yahoo.com.br>
2022-08-17 20:21:38 -0300

    Ajustando o operadores Lógicos  || (OR)

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -20,11 +20,27 @@ public class Aula04Operadores {
         boolean isDezMenorQueVinte = 10 < 20;
         boolean isDezIgualVinte = 10 == 20;
         boolean isDezIgualDez = 10 == 10;
-        boolean isDezDiferenteDez = 10 != 10;
+        boolean isDezDiferenteDez = 10 != 10.0;
         System.out.println("isDezMaiorQueVinte "+isDezMaiorQueVinte);
         System.out.println("isDezMenorQueVinte "+isDezMenorQueVinte);
         System.out.println("isDezIgualVinte "+isDezIgualVinte);
         System.out.println("isDezIgualDez "+isDezIgualDez);
         System.out.println("isDezDiferenteDez "+isDezDiferenteDez);
+
+        // Operadores Lógicos |  && (AND)  |  || (OR)   |   ! (NOT)
+        int idade = 47;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade > 40 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade > 40 && salario >= 3381;
+        System.out.println("isDentroDaLeiMaiorQueTrinta "+isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta "+isDentroDaLeiMenorQueTrinta);
+
+
+        // || operador OR ( ou )
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupaca = 10000;
+        float valorPlaystation = 5000F;
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupaca > valorPlaystation;
+        System.out.println("isPlaystationCincoCompravel "+isPlaystationCincoCompravel);
     }
 }
