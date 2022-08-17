0cc124f6bb76f71ba53dbf4dc079c0b42a941296
Janilto Oliveira <janiltojack@yahoo.com.br>
2022-08-16 22:45:58 -0300

    Inicial

---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byt, short, long, boolean
+        int idade = 100000000;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        double salarioFloat = 2500;
+        byte idadeByte = 127;
+        short idadeShort = 32000;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0040';
+        String nome = "Goku";
+
+        System.out.println("A idade é "+idade + "anos");
+        System.out.println(verdadeiro);
+        System.out.println("char" + caractere);
+        System.out.println("Oi meu nome é " + nome);
+
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.introducao;
+/*
+Prática
+Crie variáveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem
+* */
+public class Aula03TiposPrimitivosExercicio {
+}
