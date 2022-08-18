791fdf96c98ae2c0f8c3c515ae591009299d6d11
Janilto Oliveira <janiltojack@yahoo.com.br>
2022-08-17 22:19:10 -0300

    Operadores Condicionais if else

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -2,13 +2,13 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula05EstruturasCondicionais {
     public static void main(String[] args) {
-        int idade = 18;
+        int idade = 15;
         boolean isAutorizadoCompraBebida = idade >=18;
         // !
-        if (isAutorizadoCompraBebida){
-            System.out.println("Autorizado a comprar");
-        }else {
-            System.out.println("Sem autorização para comprar bebida alcólica");
+        if (isAutorizadoCompraBebida){                                          /*   usando            */
+            System.out.println("Autorizado a comprar");                         /*    IF               */
+        }else {                                                                 /*    e ELSE           */
+            System.out.println("Sem autorização para comprar bebida alcólica"); /*    Conseguimos o mesmo resultado*/
         }
         //if(isAutorizadoCompraBebida == false)   ESTA FORMA TBM ESTÁ CORRETA
         if(!isAutorizadoCompraBebida) {
