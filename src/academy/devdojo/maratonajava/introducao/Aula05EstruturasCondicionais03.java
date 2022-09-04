8085d21bfaee314ec87797ffe891e91aa0194c17
Janilto Oliveira <janiltojack@yahoo.com.br>
2022-09-04 17:53:01 -0300

    Codicionais com operador ternario

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
@@ -5,12 +5,16 @@ public class Aula05EstruturasCondicionais03 {
         double salario = 3000;
         String mensagemDoar =" Eu vou daor 500 pro DevDojo";
         String mensagemNaoVouDoar ="Ainda nao posso doar";
-        String resultado;
-      if (salario > 5000){
-          resultado = mensagemDoar;
-      }else {
-          resultado = mensagemNaoVouDoar;
-      }
+//        String resultado;
+//      if (salario > 5000){
+//          resultado = mensagemDoar;
+//      }else {
+//          resultado = mensagemNaoVouDoar;
+//      }
+
+        // AGORA COM OPERADOR TERNÁRIO
+        //(condicao) ? verdadeiro : falso
+        String resultado = salario > 5000 ? mensagemDoar : mensagemNaoVouDoar;
 
         System.out.println(resultado);
     }
