fed29e927c6eb598f2c06cae36d420cbaa523bd2
Janilto Oliveira <janiltojack@yahoo.com.br>
2022-09-04 17:34:26 -0300

    Deixando o codigo Clean

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais02.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais02.java
@@ -3,18 +3,28 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula05EstruturasCondicionais02 {
     public static void main(String[] args) {
         // if
-        // idade for <15 categoria Juvenil
+        // idade <  15 categoria Juvenil
         // idade >= 15 Infantil
         // idade >= 18 && idade < 18 categoria Juvenil
         // idade >= 18 categoria adulto
-        int idade = 17;  // aqui eu poderia criar uma query consulta em uma table para extrair o valor
-        if(idade< 15){
-            System.out.println("Categoria Infantil");
-        } else if (idade >= 15 && idade < 18){
-            System.out.println("Categoria Juvenil");
-        }else {
-            System.out.println("Categoria adulto");
-        }
 
+        int idade = 12;  // aqui eu poderia criar uma query consulta em uma table para extrair o valor
+  //      if(idade< 15){
+  //          System.out.println("Categoria Infantil");
+  //      } else if (idade >= 15 && idade < 18){
+  //          System.out.println("Categoria Juvenil");
+  //      }else {
+  //          System.out.println("Categoria adulto");
+
+        // LIMPANDO O CÓDIGO
+        String categoria;
+        if(idade< 15){
+            categoria = "Categoria Infantil";
+        } else if (idade >= 15 && idade < 18){
+            categoria = "Categoria Juvenil";
+        }else {
+            categoria = "Categoria adulto";
+        }
+        System.out.println(categoria);
     }
 }
