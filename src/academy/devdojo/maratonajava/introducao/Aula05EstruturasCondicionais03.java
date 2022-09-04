e11cf32ce7106f8a2066f453cc09c8851c8949e9
Janilto Oliveira <janiltojack@yahoo.com.br>
2022-09-04 18:04:02 -0300

    Salvando todos os metodos para futura consulta

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
@@ -1,22 +1,28 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class Aula05EstruturasCondicionais03 {
-    public static void main(String[] args) {
-        double salario = 6000;
-        String mensagemDoar =" Eu vou daor 500 pro DevDojo";
-        String mensagemNaoVouDoar ="Ainda nao posso doar";
-        String resultado;
-      if (salario > 5000){
-          resultado = mensagemDoar;
-      }else {
-          resultado = mensagemNaoVouDoar;
-      }
+       public static void main(String[] args) {
+       double salario = 6000;
+       String mensagemDoar =" Eu vou daor 500 pro DevDojo";
+       String mensagemNaoVouDoar ="Ainda nao posso doar";
 
-        // AGORA COM OPERADOR TERNÁRIO
-        //(condicao) ? verdadeiro : falso
+       String resultado = salario > 5000 ? mensagemDoar : mensagemNaoVouDoar;
 
-        System.out.println(resultado);
-    }
+       System.out.println(resultado);
+   }
+
+//    public static void main(String[] args) {
+//        double salario = 6000;
+//        String mensagemDoar =" Eu vou daor 500 pro DevDojo";
+//        String mensagemNaoVouDoar ="Ainda nao posso doar";
+//        String resultado;
+//      if (salario > 5000){
+//          resultado = mensagemDoar;
+//      }else {
+//          resultado = mensagemNaoVouDoar;
+//      }
+//        System.out.println(resultado);
+//    }
             // CONDIONAIS COM OPERADOR TERNARIO
 
 //   public static void main(String[] args) {
