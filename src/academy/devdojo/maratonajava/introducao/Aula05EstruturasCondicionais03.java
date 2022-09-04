b477a4c1698c9b5c24d92aecb246b5ee26f0c0cd
Janilto Oliveira <janiltojack@yahoo.com.br>
2022-09-04 18:02:22 -0300

    Salvando todos os metodos para futura consulta

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
@@ -2,20 +2,33 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula05EstruturasCondicionais03 {
     public static void main(String[] args) {
-        double salario = 3000;
+        double salario = 6000;
         String mensagemDoar =" Eu vou daor 500 pro DevDojo";
         String mensagemNaoVouDoar ="Ainda nao posso doar";
-//        String resultado;
-//      if (salario > 5000){
-//          resultado = mensagemDoar;
-//      }else {
-//          resultado = mensagemNaoVouDoar;
-//      }
+        String resultado;
+      if (salario > 5000){
+          resultado = mensagemDoar;
+      }else {
+          resultado = mensagemNaoVouDoar;
+      }
 
         // AGORA COM OPERADOR TERNÁRIO
         //(condicao) ? verdadeiro : falso
-        String resultado = salario > 5000 ? mensagemDoar : mensagemNaoVouDoar;
 
         System.out.println(resultado);
     }
+            // CONDIONAIS COM OPERADOR TERNARIO
+
+//   public static void main(String[] args) {
+//       double salario = 6000;
+//       String mensagemDoar =" Eu vou daor 500 pro DevDojo";
+//       String mensagemNaoVouDoar ="Ainda nao posso doar";
+//
+//       String resultado = salario > 5000 ? mensagemDoar : mensagemNaoVouDoar;
+
+//       System.out.println(resultado);
+//   }
+
+
+
 }
