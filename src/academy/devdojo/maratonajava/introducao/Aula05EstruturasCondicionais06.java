1e8bb6f1c686945918cad7435889c659abbb1605
Janilto Oliveira <janiltojack@yahoo.com.br>
2022-09-06 19:22:16 -0300

    Exercício usando switch

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais06.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais06 {
+    public static void main(String[] args) {
+        // Utilizando switch e dados os valores de 1 a 7 imprima se é o dia útil ou final de semana
+        // Considerando 1 como fim de semana
+
+    }
+}
