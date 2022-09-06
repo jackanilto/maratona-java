188528123c240e522d08866f51fe971a3100a4c0
Janilto Oliveira <janiltojack@yahoo.com.br>
2022-09-06 08:20:30 -0300

    Exercício proposto feito (Modificando o cod)

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais04.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais04.java
@@ -1,22 +1,28 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class Aula05EstruturasCondicionais04 {
+
+  //  € 0 - 19.982	    36,55%*	€ 7.303
+  //  € 33.792 - 67.072	40,80%	€ 13.578
+  //  € 67.073+       	52%
+
     public static void main(String[] args) {
 
-        double salario = 33791;
+        double salario = 19.992;
         double taxa1 = 36.55 / 100;
         double taxa2 = 40.80 / 100;
-        double taxa4 = 52.00 / 100;
+        double taxa3 = 52.00 / 100;
+        double taxaAnual;
 
-        String taxaAnual = "";
-        if (salario >= 19982){
-            taxaAnual = String.valueOf(salario * taxa1);
-        } else if (salario > 19982 && salario < 33791) {
-            salario *= taxa2; // salario = salario * taxa4;
+     //   String taxaAnual = "";
+        if (salario <= 19982){
+            taxaAnual = salario * taxa1;
+        } else if (salario >= 19982 && salario <= 33791) {
+            taxaAnual = salario * taxa2; // salario = salario * taxa3;
         }else {
-            salario *= taxa4; // salario = salario * taxa4;
+            taxaAnual = salario * taxa3; // salario = salario * taxa3;
         }
-        System.out.println("Valor da taxa anual: "+taxaAnual);
+        System.out.println("Valor da taxa anual: "+ taxaAnual);
 
     }
 }
