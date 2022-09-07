5febdd8e1e51127368f7335e501c5e5f3dee9a88
Janilto Oliveira <janiltojack@yahoo.com.br>
2022-09-06 22:00:27 -0300

    Repetição While count++

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao {
+    public static void main(String[] args) {
+        //While, do while, for
+        int count = 0;
+        while (count < 10){
+            System.out.println(count);
+            count ++;
+
+        }
+    }
+
+
+}
