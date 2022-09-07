895d50035f33f25a377946347e208737ee522233
Janilto Oliveira <janiltojack@yahoo.com.br>
2022-09-07 09:41:51 -0300

    Exercicio: Contar de 0 a 25 de 50

--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao03.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao03.java
@@ -4,8 +4,8 @@ public class Aula06EstruturasDeRepeticao03 {
     // Imprima os primeiros 25 numero de um dado valor ex: 50
     public static void main(String[] args) {
         int countMax = 50;
-        for (int i = 0; i <= countMax ; i++) {
-            if (i > 25){
+        for (int i = 0; i <= countMax; i++) {
+            if (i > 25) {
                 break;
             }
             System.out.println(i);
