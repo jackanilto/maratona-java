a7f31fbae60f0d29a6211d441a789981b3f445ca
Janilto Oliveira <janiltojack@yahoo.com.br>
2022-09-06 22:54:35 -0300

    Exercicio: Contar de 0 a 1M numeros pares

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao02.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao02 {
+    public static void main(String[] args) {
+        //While, do while, for
+        int count = 1; // Iniciando com 0 irá contar os pares / com 1 irá contar os impares
+        while (count < 1000000) {
+            System.out.println(count +=2);
+
+            }
+        }
+    }
+
+
