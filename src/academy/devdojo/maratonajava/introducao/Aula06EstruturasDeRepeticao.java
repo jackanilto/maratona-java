b3665e128281b42c3341da614cbd6d095706fbb6
Janilto Oliveira <janiltojack@yahoo.com.br>
2022-09-06 22:12:13 -0300

    Testando for

--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao.java
@@ -4,13 +4,19 @@ public class Aula06EstruturasDeRepeticao {
     public static void main(String[] args) {
         //While, do while, for
         int count = 0;
-        while (count < 10){
+        while (count < 10) {
             System.out.println(++count);
 
         }
-        do{
-            System.out.println();
+        count = 0;
+        do {
+            System.out.println("Dentro do do-while" + ++count);
+
+        } while (count < 10);
+        for (count = 0; count < 10; count++) {
+            System.out.println("For " +count);
         }
+
     }
 
 
