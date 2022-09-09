1d3ba464f55ac2a1968e5ce26012510117ef3b5f
Janilto Oliveira <janiltojack@yahoo.com.br>
2022-09-08 23:55:01 -0300

    Arrays com 3 tipo references

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -7,14 +7,28 @@ public class Aula07Arrays02 {
         // boolean false
         //String null
 
-        String [] nomes = new String[4];
+        System.out.println("++++++++++++++Exercicio 01 +++++++++++++++++");
+
+        String[] nomes = new String[4];
         nomes[0] = "Antonio";
         nomes[1] = "Jose";
         nomes[2] = "Ailton";
+        //  nomes[3] = "Ailto";
 
-        for (int i = 0; i < 4 ; i++) {
+        for (int i = 0; i < nomes.length; i++) {
             System.out.println(nomes[i]);
-
         }
+
+        System.out.println("++++++++++++++Exercicio 02 +++++++++++++++++");
+
+
+        int[] idadeCadastrados = {100, 200, 300, 400};
+        int[] idades = new int[4];
+        int i;
+        for (i = 0; i < idades.length; i++) {
+            idades[i] = idadeCadastrados[i];
+            System.out.println(idades[i]);
+        }
+
     }
 }
