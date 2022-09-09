069e3c62608688e2b6598a6d07a9db2269feea9f
Janilto Oliveira <janiltojack@yahoo.com.br>
2022-09-08 23:18:32 -0300

    Arrays com 3 references

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
@@ -2,7 +2,14 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula07Arrays01 {
     public static void main(String[] args) {
-        int [] idades = null;
-        System.out.println(idades);
+        int [] idades = new int[3];
+        idades[0] = 21;
+        idades[1] = 30;
+        idades[2] = 45;
+
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
     }
 }
+
